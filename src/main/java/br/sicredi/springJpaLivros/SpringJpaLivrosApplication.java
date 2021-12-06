2563db343d623ac24c584c5c07aca420680e25a6
Maria Manoela <manoelamvc@hotmail.com>
2021-12-06 15:40:03 -0300

    :tada: First commit

---
+++ src/main/java/br/sicredi/springJpaLivros/SpringJpaLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.sicredi.springJpaLivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/sicredi/springJpaLivros/SpringJpaLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sicredi.springJpaLivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
