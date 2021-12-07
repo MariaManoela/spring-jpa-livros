5836db969a04d34805eee008cc9979b3cce0ee26
Maria Manoela <manoelamvc@hotmail.com>
2021-12-07 09:56:57 -0300

    Adding controller and service methods

--- src/main/java/br/sicredi/springJpaLivros/controller/EditoraController.java
+++ src/main/java/br/sicredi/springJpaLivros/controller/EditoraController.java
@@ -1,14 +1,32 @@
 package br.sicredi.springJpaLivros.controller;
 
+import br.sicredi.springJpaLivros.model.Editora;
 import br.sicredi.springJpaLivros.repository.EditoraRepository;
 import br.sicredi.springJpaLivros.service.EditoraService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/editoras")
 public class EditoraController {
     @Autowired
     EditoraService editoraService;
+
+    @GetMapping
+    public List<Editora> findAll() {
+        return editoraService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional findById(@PathVariable("id") Long id) {
+        return editoraService.findById(id);
+    }
+
+    @PostMapping
+    public void cadastraEditora(@RequestBody Editora editora) {
+        editoraService.save(editora);
+    }
 }
--- src/main/java/br/sicredi/springJpaLivros/service/EditoraService.java
+++ src/main/java/br/sicredi/springJpaLivros/service/EditoraService.java
@@ -1,11 +1,27 @@
 package br.sicredi.springJpaLivros.service;
 
+import br.sicredi.springJpaLivros.model.Editora;
 import br.sicredi.springJpaLivros.repository.EditoraRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class EditoraService {
     @Autowired
     EditoraRepository editoraRepository;
+
+    public List<Editora> findAll() {
+        return  editoraRepository.findAll();
+    }
+
+    public Optional findById(Long id) {
+        return editoraRepository.findById(id);
+    }
+
+    public void save(Editora editora) {
+        editoraRepository.save(editora);
+    }
 }
